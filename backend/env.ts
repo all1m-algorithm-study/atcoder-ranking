12cd708f2309a0321beb5ce0a32063e857c82a55
Jeonghyeon Kim <powergee101@gmail.com>
2021-07-28 19:14:03 +0900

    Add environment variable to backend: CLIENT_HOST

--- backend/env.ts
+++ backend/env.ts
@@ -5,7 +5,8 @@ interface IEnv {
     port: string,
     mongoURI: string,
     accessTokenKey: string,
-    adminPassword: string
+    adminPassword: string,
+    clientHost: string
 }
 
 if (process.env.PORT === undefined) {
@@ -18,14 +19,18 @@ if (process.env.ACCESS_SECRET_KEY === undefined) {
     throw new Error("ACCESS_SECRET_KEY is not set.");
 }
 if (process.env.ADMIN_PASSWORD === undefined) {
-    throw new Error("ADMIN_PASSWORD is not set.")
+    throw new Error("ADMIN_PASSWORD is not set.");
+}
+if (process.env.CLIENT_HOST === undefined) {
+    throw new Error("CLIENT_HOST is not set.");
 }
 
 const env:IEnv = {
     port: process.env.PORT,
     mongoURI: process.env.MONGO_URI,
     accessTokenKey: process.env.ACCESS_SECRET_KEY,
-    adminPassword: process.env.ADMIN_PASSWORD
+    adminPassword: process.env.ADMIN_PASSWORD,
+    clientHost: process.env.CLIENT_HOST
 }
 
 export default env
